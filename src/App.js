1982d83c5449f1f97591d25abba3393b4fcd502e
Isabele Rau <bebel.candido@gmail.com>
2025-10-19 20:45:19 -0300

    Criação do projeto e integracao com os componentes desenvolvidos na POC do Stencil

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+   useEffect(() => {
+    const component = document.querySelector('my-component');
+    component.addEventListener('nameChanged', e => {
+      console.log('From React:', e.detail);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React with POC Stencil</h1>
+      <my-component name="React user: Isabele"></my-component>
     </div>
   );
 }
